c497418a1313e6bc465065749976b85d735752d2
V8tr <ne0_n@mail.ru>
2020-02-05 17:34:52 +0200

    Initial commit

---
+++ ListCRUDSwiftUI/ContentView.swift
@@ -0,0 +1,78 @@
+//
+//  ContentView.swift
+//  ListCRUDSwiftUI
+//
+//  Created by Vadym Bulavin on 2/5/20.
+//  Copyright © 2020 Vadym Bulavin. All rights reserved.
+//
+
+import SwiftUI
+
+struct Item: Identifiable, Hashable {
+    let id: Int
+    let title: String
+    
+    static func samples() -> [Item] { (0..<3).map { Item(id: $0, title: "Item #\($0)") } }
+}
+
+struct ContentView: View {
+    @State var items: [Item]
+    @State var selection: Set<Item.ID> = []
+    @State var editMode: EditMode = .inactive
+    
+    init(items: [Item]) {
+        _items = State(initialValue: items)
+    }
+    
+    var body: some View {
+        NavigationView {
+            List(items, selection: $selection) { item in
+                Text(item.title)
+            }
+            .navigationBarTitle(Text("CRUD List"))
+            .navigationBarItems(leading: editOrDoneButton, trailing: addOrDeleteButton)
+            .environment(\.editMode, $editMode)
+        }
+    }
+    
+    private var editOrDoneButton: some View {
+        switch editMode {
+        case .inactive:
+            return Button(action: onEdit) { Text("Edit") }
+        default:
+            return Button(action: onDone) { Text("Done") }
+        }
+    }
+    
+    private var addOrDeleteButton: some View {
+        switch editMode {
+        case .inactive:
+            return Button(action: onAdd) { Image(systemName: "plus") }
+        default:
+            return Button(action: onDelete) { Image(systemName: "trash") }
+        }
+    }
+    
+    private func onEdit() {
+        editMode = .active
+    }
+    
+    private func onDone() {
+        editMode = .inactive
+    }
+    
+    private func onAdd() {
+        items.append(Item(id: items.count, title: "Item #\(items.count)"))
+    }
+    
+    private func onDelete() {
+        items.removeAll { item in selection.contains { $0 == item.id } }
+        selection = []
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(items: [])
+    }
+}
---
+++ ListCRUDSwiftUI/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  ListCRUDSwiftUI
+//
+//  Created by Vadym Bulavin on 2/5/20.
+//  Copyright © 2020 Vadym Bulavin. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: ContentView(items: Item.samples()))
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+}
+
